01bcbdf12cdb03e5f182e648eb454c7c638fefb3
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2026-03-19 16:16:38 +0000

    feat: define `create_maze()`

--- data_generation/maze.py
+++ data_generation/maze.py
@@ -2,74 +2,77 @@ import bpy
 import bmesh
 import random
 
-active = bpy.context.active_object
 
-if active.type != 'MESH':  # type: ignore
-    raise Exception("Active object must be a mesh object")
+def create_maze(grid_obj):
+    if grid_obj.type != 'MESH':  # type: ignore
+        raise Exception("Must be a mesh object")
 
-assert (active is not None)
-assert (active.modifiers is not None)
+    assert (grid_obj is not None)
+    assert (grid_obj.modifiers is not None)
 
-# Verify active object has maze nodes modifier
-maze_nodes = bpy.data.node_groups['Maze']
-maze_modifier = None
+    # Verify object has maze nodes modifier
+    maze_nodes = bpy.data.node_groups['Maze']
+    maze_modifier = None
 
-for modifier in active.modifiers:
-    if modifier.type == 'NODES' and modifier.node_group == maze_nodes:
-        maze_modifier = modifier
-        break
+    for modifier in grid_obj.modifiers:
+        if modifier.type == 'NODES' and modifier.node_group == maze_nodes:
+            maze_modifier = modifier
+            break
 
-if not maze_modifier:
-    maze_modifier = active.modifiers.new('Maze', 'NODES')  # type: ignore
-    maze_modifier.node_group = bpy.data.node_groups['Maze']
-    for i in range(len(active.modifiers) - 1):  # type: ignore
-        bpy.ops.object.modifier_move_up(
-            modifier=maze_modifier.name)  # type: ignore
+    if not maze_modifier:
+        maze_modifier = grid_obj.modifiers.new('Maze', 'NODES')  # type: ignore
+        maze_modifier.node_group = bpy.data.node_groups['Maze']
+        for i in range(len(grid_obj.modifiers) - 1):  # type: ignore
+            bpy.ops.object.modifier_move_up(
+                modifier=maze_modifier.name)  # type: ignore
 
+    # Verify object mesh has 'closed' attribute
+    mesh = grid_obj.data
+    bm = bmesh.new()
+    bm.from_mesh(mesh)  # type: ignore
+    closed = mesh.attributes.get('closed')  # type: ignore
+    if not closed:
+        closed = mesh.attributes.new(  # type: ignore
+            name='closed', type='BOOLEAN', domain='EDGE'
+        )
+        closed.name = 'closed'
 
-# Verify active mesh has 'closed' attribute
-mesh = active.data
-bm = bmesh.new()
-bm.from_mesh(mesh)  # type: ignore
-closed = mesh.attributes.get('closed')  # type: ignore
-if not closed:
-    closed = mesh.attributes.new(  # type: ignore
-        name='closed', type='BOOLEAN', domain='EDGE'
-    )
-    closed.name = 'closed'
+    # Reset the maze by closing all edges
+    for edge in bm.edges:
+        setattr(closed.data[edge.index], 'value', True)
 
+    # Randomly chose a cell to start carving the maze from
+    current_cell = random.choice(list(bm.faces))
+    visited = [current_cell]
+    stack = [current_cell]
 
-# Reset the maze by closing all edges
-for e in bm.edges:
-    setattr(closed.data[e.index], 'value', True)
+    # Loop until the entire maze is carved out
+    while len(stack) > 0:
+        current_cell = stack.pop()
 
-# Randomly chose a cell to start carving the maze from
-current_cell = random.choice(list(bm.faces))
-visited = [current_cell]
-stack = [current_cell]
-
-# Loop until the entire maze is carved out
-while len(stack) > 0:
-    current_cell = stack.pop()
-
-    # Get unvisited neighbors to current cell
-    unvisited_neighbors = []
-    for edge in current_cell.edges:
-        for neighbor in edge.link_faces:
-            if neighbor != current_cell and neighbor not in visited:
-                unvisited_neighbors.append(neighbor)
-
-    # Randomly choose an unvisited neighbor
-    if len(unvisited_neighbors) > 0:
-        stack.append(current_cell)
-        chosen_neighbor = random.choice(unvisited_neighbors)
-        visited.append(chosen_neighbor)
-        stack.append(chosen_neighbor)
-
-        # open all edges between current cell and chosen neighbor
+        # Get unvisited neighbors to current cell
+        unvisited_neighbors = []
         for edge in current_cell.edges:
-            if edge in chosen_neighbor.edges:
-                setattr(closed.data[edge.index], 'value', False)
+            for neighbor in edge.link_faces:
+                if neighbor != current_cell and neighbor not in visited:
+                    unvisited_neighbors.append(neighbor)
 
-# update the mesh to show the maze
-mesh.update()  # type: ignore
+        # Randomly choose an unvisited neighbor
+        if len(unvisited_neighbors) > 0:
+            stack.append(current_cell)
+            chosen_neighbor = random.choice(unvisited_neighbors)
+            visited.append(chosen_neighbor)
+            stack.append(chosen_neighbor)
+
+            # open all edges between current cell and chosen neighbor
+            for edge in current_cell.edges:
+                if edge in chosen_neighbor.edges:
+                    setattr(closed.data[edge.index], 'value', False)
+
+    # update the mesh to show the maze
+    mesh.update()  # type: ignore
+
+
+# Call the function
+assert (bpy.context.scene is not None)
+create_maze(bpy.context.scene.objects['Grid'])
