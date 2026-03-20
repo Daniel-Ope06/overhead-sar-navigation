9c7c8dba985562a742c0d9da58567f348619fdde
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2026-03-20 16:09:33 +0000

    chore: test run `PathFinder` class

--- cv_pipeline/main.py
+++ cv_pipeline/main.py
@@ -3,6 +3,7 @@ import os
 
 from modules.target_detector import TargetDetector
 from modules.color_segmenter import ColorSegmenter
+from modules.path_finder import PathFinder
 
 
 def main():
@@ -17,16 +18,24 @@ def main():
     GRID_RESOLUTION = 30
     detector = TargetDetector(grid_size=GRID_RESOLUTION)
     segmenter = ColorSegmenter(grid_size=GRID_RESOLUTION)
+    path_finder = PathFinder()
 
     print("\n--- Target Detection ---")
     try:
         targets = detector.fake_detect(IMG_PATH, LBL_PATH)
-        for cls_id, data in targets.items():
-            name = "UGV" if cls_id == 0 else "Human Target"
-            print(f"[+] {name} Located:")
-            print(f"    - Pixel Start (Top-Left): {data['bbox_top_left']}")
-            print(f"    - Bounding Box Size: {data['bbox_size']}")
-            print(f"    - Mapped to Matrix Node: {data['grid_node']}")
+
+        ugv_data = targets.get(0)
+        human_data = targets.get(1)
+
+        if not ugv_data or not human_data:
+            print("Error: Could not locate both UGV and Human ")
+            return
+
+        start_node = ugv_data['grid_node']
+        goal_node = human_data['grid_node']
+
+        print(f"[+] UGV located at matrix node: {start_node}")
+        print(f"[+] Human located at matrix node: {goal_node}")
     except Exception as e:
         print(f"[!] Detection Error: {e}")
         return
@@ -34,14 +43,26 @@ def main():
     print("\n--- Color Segmentation ---")
     # segmenter.tune_thresholds(IMG_PATH)
     try:
-        grid, original_img = segmenter.generate_matrix(IMG_PATH)
-        print(f"[+] Matrix successfully generated! Shape: {grid.shape}")
-
-        print("\n[+] Snippet of the Top-Left Matrix (10x10):")
-        print(grid[0:10, 0:10])
-
+        binary_matrix, original_img = segmenter.generate_matrix(IMG_PATH)
+        print(f"Matrix Shape: {binary_matrix.shape}")
     except Exception as e:
         print(f"[!] Segmentation Error: {e}")
+        return
+
+    print("\n--- Path Finding ---")
+    try:
+        print("[*] Calculating optimal route...")
+        optimal_path = path_finder.find_path(
+            binary_matrix, start_node, goal_node)
+
+        if not optimal_path:
+            print("[!] ROUTING FAILED: No valid path exists.")
+        else:
+            print(f"Path found in {len(optimal_path)} steps!")
+            print(f"(First 5 steps): {optimal_path[:5]} ...")
+            print(f"(Last 5 steps): ... {optimal_path[-5:]}")
+    except Exception as e:
+        print(f"[!] Pathfinding Error: {e}")
 
 
 if __name__ == "__main__":
